93ec7685b914bd73942d24beca9ad9985e2bd779
elroy-He <elroyhsc@gmail.com>
2023-11-06 17:24:07 -0800

    fixed lint error

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -25,7 +25,8 @@ class CommandTest(SimpleTestCase):
     @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """testing waiting for database when getting operationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        psycopg2_errors = [Psycopg2Error] * 2 + [OperationalError] * 3
+        patched_check.side_effect = psycopg2_errors + [True]
 
         call_command("wait_for_db")
         self.assertEqual(patched_check.call_count, 6)
